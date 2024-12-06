09f7ad1d7f82abe8298416695445cb4341a5a690
pronoy656 <pronoypaul56@gmail.com>
2024-12-06 13:03:43 +0600

    Navbar and Footer Impliment

--- src/Component/Root/Root.jsx
+++ src/Component/Root/Root.jsx
@@ -1,7 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Shared/Navbar/Navbar";
+import Footer from "../Shared/Footer/Footer";
+
 const Root = () => {
   return (
     <div>
       <h1>This is Root</h1>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
