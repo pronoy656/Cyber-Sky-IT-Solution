8b2ab7c040e49cc16a2ea7c9b87b43d52870018a
pronoy656 <pronoypaul56@gmail.com>
2024-12-06 19:43:51 +0600

    image correction in json data and load data from json file

---
+++ src/Component/Pages/Home/FruitCategory.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const FruitCategory = ({ fruit }) => {
+  const { name, iconImage, vitaminType } = fruit;
+  return (
+    <div>
+      <div className="border flex  items-center space-x-3">
+        <img className="w-8 h-8 border rounded-full" src={iconImage} alt="" />
+        <div>
+          <h1 className="text-lg font-semibold">{name}</h1>
+          <h1 className="font-medium text">{vitaminType}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FruitCategory;
--- src/Component/Pages/Home/Home.jsx
+++ src/Component/Pages/Home/Home.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import FruitCategory from "./FruitCategory";
 
 const Home = () => {
+  const [fruits, setFruits] = useState([]);
+
+  useEffect(() => {
+    fetch("fuits.json")
+      .then((res) => res.json())
+      .then((data) => setFruits(data));
+  }, []);
   return (
     <div>
       <h1>This is Home page</h1>
       <div className="flex space-x-4">
         <div className="w-40 border border-red-400">
-          <h1>This is left side</h1>
+          <h1>Fruits list</h1>
+          <h1>Total Items: {fruits.length}</h1>
+          <div className="border">
+            {fruits.map((fruit) => (
+              <FruitCategory key={fruit.id} fruit={fruit}></FruitCategory>
+            ))}
+          </div>
         </div>
         <div className="w-[1100px] border border-blue-600">
           <h1>This is middle side</h1>
