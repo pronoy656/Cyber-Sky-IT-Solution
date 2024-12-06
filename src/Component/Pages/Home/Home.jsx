3543cf86186adf076b016d2a82a725b953de34e4
pronoy656 <pronoypaul56@gmail.com>
2024-12-06 14:32:29 +0600

    allgn setup in home page

--- src/Component/Pages/Home/Home.jsx
+++ src/Component/Pages/Home/Home.jsx
@@ -4,6 +4,17 @@ const Home = () => {
   return (
     <div>
       <h1>This is Home page</h1>
+      <div className="flex space-x-4">
+        <div className="w-40 border border-red-400">
+          <h1>This is left side</h1>
+        </div>
+        <div className="w-[1100px] border border-blue-600">
+          <h1>This is middle side</h1>
+        </div>
+        <div className="w-40 border border-red-400">
+          <h1>This is Right side</h1>
+        </div>
+      </div>
     </div>
   );
 };
